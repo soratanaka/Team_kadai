5c9b2dce592d6887ec57b21a0e12a6f4bb4eb2d7
田中宙 <tanakachuu@tanakachuunoAir.elecom>
2022-11-13 12:55:09 +0900

    #2 crear

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,6 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_team, only: %i[show edit update destroy]
+  before_action :set_team, only: %i[show edit update destroy ]
 
   def index
     @teams = Team.all
@@ -30,6 +30,7 @@ class TeamsController < ApplicationController
   end
 
   def update
+    byebug
     if @team.update(team_params)
       redirect_to @team, notice: I18n.t('views.messages.update_team')
     else
@@ -47,6 +48,12 @@ class TeamsController < ApplicationController
     @team = current_user.keep_team_id ? Team.find(current_user.keep_team_id) : current_user.teams.first
   end
 
+  def chengowner
+    @team = Team.friendly.find(params[:id])
+    @team.update(owner_id:params[:format])
+    redirect_to @team, notice: I18n.t('views.messages.update_team')
+  end
+
   private
 
   def set_team
@@ -54,6 +61,6 @@ class TeamsController < ApplicationController
   end
 
   def team_params
-    params.fetch(:team, {}).permit %i[name icon icon_cache owner_id keep_team_id]
+    params.fetch(:team, {}).permit %i[name icon icon_cache owner_id keep_team_id ]
   end
 end
